368a3a03a155effdfc346b1e01a2d51016a8bf9d
Adit <gautamadit@gmail.com>
2025-10-24 14:13:29 -0700

    added nav menu js

--- global.js
+++ global.js
@@ -11,3 +11,40 @@ const currentLink = navLinks.find(
 );
 
 currentLink?.classList.add("current");
+
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "CV/", title: "CV" },
+  { url: "https://github.com/aditgautam", title: "GitHub" },
+];
+
+let BASE_PATH =
+  (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"
+    : "/portfolio/";
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  url = !url.startsWith("http") ? BASE_PATH + url : url;
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = p.title;
+
+  a.classList.toggle(
+    "current",
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+    a.rel = "noopener";
+  }
+
+  nav.append(a);
+}
